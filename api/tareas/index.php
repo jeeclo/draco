2317d8899b5b585a8cd39a53e64e314a4c9b5c2f
jeeclo <isc.jesus.e@gmail.com>
2020-08-18 12:04:39 -0500

    Index - EC

--- api/tareas/index.php
+++ api/tareas/index.php
@@ -1,3 +1,25 @@
 <?php
 
+
+switch ($variable) {
+    case 'POST':
+        # code...
+        break;
+    
+    case 'PUT':
+        # code...
+        break;
+    
+    case 'GET':
+        # code...
+        break;
+    
+    case 'DELETE':
+        # code...
+        break;
+    
+}
+
+
+
 ?>
